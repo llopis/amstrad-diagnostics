3b60ce8a74f3723db4c8dd1243ede59021dfa1e2
llopis <llopis@gmail.com>
2021-03-01 21:45:33 +0100

    Resetting screen completely before printing to it

--- src/Screen.asm
+++ src/Screen.asm
@@ -1,5 +1,37 @@
 
+CRTCDefaultTable:
+	db 63                 ;* R0 = 63     : Horizontal Total
+	db 40                 ;* R1 = 40     : Horizontal Displayed
+	db 46                 ;* R2 = 46     : Horizontal Sync Position
+	db 142                ;* R3 = 8*16+14 : Horizontal and Vertical Sync Widths
+	db 38                 ;* R4 = 38     : Vertical Total
+	db 0                  ;* R5 =  0     : Vertical Total Adjust
+	db 25                 ;* R6 = 25     : Vertical Displayed
+	db 30                 ;* R7 = 30     : Vertical Sync position
+	db 0                  ;* R8 =  0     : Interlace and Skew
+	db 7                  ;* R9 =  7     : Maximum Raster Address
+	db 0                  ;R10 
+	db 0                  ;R11
+	db #30                ;*R12 =        : Display Start Address (#C000 / 16k)
+	db #00                ;*R13 =  0     : Display Start Address 
+	db #C0                ;R14
+	db #00                ;R15
+CRTCDefaultTableEnd:
+
 ClearScreen:
+	;; Initialize Display
+	ld hl, CRTCDefaultTableEnd
+	ld bc, #BC0F      ;; CRTC select 0F
+.crtcLoop:
+	out (c),c        
+	dec hl
+	ld a,(hl)
+	inc b
+	out (c),a
+	dec b
+	dec c
+	jp p,.crtcLoop
+
 	ld hl,#C000
 	ld de,#C001
 	ld bc,#4000
