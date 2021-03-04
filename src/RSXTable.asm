f851995dbeb80bfbf69e144fa21ddf2538513fe1
George Schizas <gschizas@gmail.com>
2021-03-04 22:24:36 +0200

    Fix ROM format

--- src/RSXTable.asm
+++ src/RSXTable.asm
@@ -1,13 +1,35 @@
 	; ROM header
-	db #00, #00, #01, #00
+	db #01, #00, #00, #05
 
 RSXTable:
 	dw RSXNames
+	jp Bootup
 	jp LowerRAMTest
 
 
 RSXNames:
-	db "DIA", 'G' | 0x80
+	dc 'AmsDiag'
+	dc 'DIAG'
 	db 0                     ;end of name table marker
 
 
+Bootup:
+		PUSH AF
+		PUSH BC
+		PUSH DE
+		PUSH HL
+		LD   HL, game_message
+loop_show_message:
+		LD   A,(HL)
+		CALL $BB5A                      ; TXT_OUTPUT
+		INC  HL
+		OR	 A
+		JR   NZ,loop_show_message
+		POP  HL
+		POP  DE
+		POP  BC
+		POP  AF
+		RET
+
+game_message
+    db " Amstrad Diagnostics ",164," Noel Llopis 2021",13,10,13,10,0
