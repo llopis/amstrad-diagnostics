6aca6817c7692d98f2387620d1eb71287bf73032
llopis <llopis@gmail.com>
2021-03-07 10:51:00 +0100

    Building packaging RAM build in tape (CDT) as well as DSK

--- build.sh
+++ build.sh
@@ -8,4 +8,5 @@ sjasmplus --nologo --msg=war -DRAMBuild=1 --raw=build/diag.bin --lst=build/Amstr
 sjasmplus --nologo --msg=war -DLowerROMBuild=1 --raw=build/AmstradDiagLower.rom --lst=build/AmstradDiagLower_list.txt  src/Main.asm
 iDSK -n build/AmstradDiag.dsk > /dev/null 2>&1
 iDSK build/AmstradDiag.dsk -e 400 -c 400 -t 1 -i build/diag.bin > /dev/null 2>&1
-cd build; zip -q AmstradDiag.zip *.rom *.cpr *.dsk ; cd ..
+2cdt -F 2 -X 0x400 -L 0x400 -r diag.bin -n build/diag.bin build/AmstradDiag.cdt > /dev/null 2>&1
+cd build; zip -q AmstradDiag.zip *.rom *.cpr *.dsk *.cdt ; cd ..
